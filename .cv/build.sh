2584b918569cefd3ce140cdd60f973593af8fb5c
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2026-07-01 06:13:39 -0400

    build: move site to docs/

--- .cv/build.sh
+++ .cv/build.sh
@@ -5,17 +5,17 @@ set -eo pipefail
 cd "$(dirname "$0")"
 
 # HTML
-pandoc ../cv/downey_simon_engineer_cv.md -o ../cv/index.html \
+pandoc ../docs/cv/downey_simon_engineer_cv.md -o ../docs/cv/index.html \
   --template=template.html \
   --metadata title="Simon Downey - CV" \
   -f markdown-citations \
   --lua-filter=external-links.lua \
   --wrap=none
 
-tidy -i -w 0 -q -m --tidy-mark no ../cv/index.html 2>/dev/null || true
+tidy -i -w 0 -q -m --tidy-mark no ../docs/cv/index.html 2>/dev/null || true
 
 # PDF
-pandoc ../cv/downey_simon_engineer_cv.md \
+pandoc ../docs/cv/downey_simon_engineer_cv.md \
   --template=template.html \
   --metadata title="Simon Downey - CV" \
   -f markdown-citations \
@@ -23,4 +23,4 @@ pandoc ../cv/downey_simon_engineer_cv.md \
   --lua-filter=two-column.lua \
   --wrap=none \
   -o - \
-  | weasyprint -s pdf.css --base-url ../cv/ - ../cv/downey_simon_engineer_cv.pdf
+  | weasyprint -s pdf.css --base-url ../docs/cv/ - ../docs/cv/downey_simon_engineer_cv.pdf
